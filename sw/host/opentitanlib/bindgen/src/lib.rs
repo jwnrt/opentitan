fd7bcb53e03a34ab64f8903610a6663e63d8b677
James Wainwright <james.wainwright@lowrisc.org>
2023-06-15 17:07:30 +0100

    [opentitanlib] Prepare for build script for bindgen
    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>

--- sw/host/opentitanlib/bindgen/src/lib.rs
+++ sw/host/opentitanlib/bindgen/src/lib.rs
@@ -2,8 +2,22 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-pub use alert;
-pub use dif;
-pub use earlgrey;
-pub use hardened;
-pub use multibits;
+pub mod alert {
+    include!(concat!(env!(OUT_DIR), "alert.rs"));
+}
+
+pub mod dif {
+    include!(concat!(env!("OUT_DIR"), "dif.rs"));
+}
+
+pub mod earlgrey {
+    include!(concat!(env!("OUT_DIR"), "earlgrey.rs"));
+}
+
+pub mod hardened {
+    include!(concat!(env!("OUT_DIR"), "hardened.rs"));
+}
+
+pub mod multibits {
+    include!(concat!(env!("OUT_DIR"), "multibits.rs"));
+}
